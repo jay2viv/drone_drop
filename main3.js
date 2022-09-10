2f4eeaf01cfbbdb47b6871c1e8d60797b6b0a2c8
jay2viv <97814633+jay2viv@users.noreply.github.com>
2022-09-10 11:07:24 -0400

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,14 @@
+function friedrice()
+{
+    window.location = "index4.html";
+}
+
+function dessert_kidmeal()
+{
+    window.location = "index5.html";
+}
+
+function curry()
+{
+    window.location.replace("index3.html");
+}
